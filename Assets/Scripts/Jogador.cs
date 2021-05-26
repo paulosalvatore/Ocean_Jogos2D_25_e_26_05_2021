0cc45eb2fcc22fd8e4796bb60c255b08de776695
Paulo Salvatore <salvatoregames@gmail.com>
2021-05-26 11:51:25 -0300

    Método atirar do jogador

--- Assets/Scripts/Jogador.cs
+++ Assets/Scripts/Jogador.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,8 @@ using UnityEngine;
 public class Jogador : MonoBehaviour
 {
     public float velocidade = 2f;
+
+    public GameObject projetilPrefab;
 
     // Start é chamado uma vez assim que o jogo inicia
     void Start()
@@ -15,6 +18,33 @@ public class Jogador : MonoBehaviour
     // Update é chamado o tempo todo enquanto o jogo estiver rodando
     // O número de vezes que ele é chamado depende do FPS que o jogo está rodando
     void Update()
+    {
+        Movimentar();
+
+        Atirar();
+    }
+
+    private void Atirar()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // print("Atirar!!!");
+
+            var projetilClone = Instantiate(projetilPrefab);
+
+            projetilClone.transform.position = transform.position;
+
+            var mousePosition = Input.mousePosition;
+            mousePosition.z = -Camera.main.transform.position.z;
+
+            var worldMousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            var direcao = (worldMousePosition - transform.position).normalized;
+
+            projetilClone.transform.up = direcao;
+        }
+    }
+
+    private void Movimentar()
     {
         // Pegar movimento das setas (horizontal e vertical)
         // Aplicar esse valor na posição do jogador
