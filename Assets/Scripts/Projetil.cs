9e110c71ff0a4e0ddeb499f7b5d05f743d344646
Paulo Salvatore <salvatoregames@gmail.com>
2021-05-26 12:42:11 -0300

    Destruindo o projétil após um tempo

--- Assets/Scripts/Projetil.cs
+++ Assets/Scripts/Projetil.cs
@@ -6,6 +6,13 @@ public class Projetil : MonoBehaviour
 {
     public float velocidade = 20f;
 
+    public float delayDestruir = 5f;
+
+    void Start()
+    {
+        Destroy(gameObject, delayDestruir);
+    }
+
     void Update()
     {
         var direcao = Vector2.up;
